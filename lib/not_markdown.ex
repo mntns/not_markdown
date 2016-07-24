7df2661171eff96829b114056618ee90bfd16b70
Eddy Shure <github@monoton.space>
2016-07-24 21:36:45 +0200

    Implement more features following the spec

--- lib/not_markdown.ex
+++ lib/not_markdown.ex
@@ -1,24 +1,27 @@
 defmodule NotMarkdown do
 
-  @bold_rgx ~r/\*(.+?)\*/
-  @italics_rgx ~r/_(.+?)_/
-  @strikethrough_rgx ~r/~(.+?)~/
-
   def to_html(string) do
     string
-    |> apply_bold
-    |> apply_italics
+    |> outer_replace(~r/^>{3}(.+?)\z/sm, "blockquote")
+    |> outer_replace(~r/^>(.+?)$/sm, "blockquote")
+    |> outer_replace(~r/\*(.+?)\*/, "b")
+    |> outer_replace(~r/_(.+?)_/, "i")
+    |> outer_replace(~r/~(.+?)~/, "s")
+    |> outer_replace(~r/`{3}(.+?)`{3}/sm, "pre")
+    |> outer_replace(~r/`(.+?)`/, "code")
+    |> create_lists()
   end
 
-  def apply_bold(str) do
-    Regex.replace(@bold_rgx, str, fn _, x -> "<b>#{x}</b>" end)
+  defp create_lists(text) do
+    # TODO: list creation (ol/ul)
+    text
+  end
+    
+  defp outer_replace(string, regex, tag) do
+    Regex.replace(regex, string, fn _, x -> build_tag(x, tag) end)
   end
 
-  def apply_italics(str) do
-    Regex.replace(@italics_rgx, str, fn _, x -> "<i>#{x}</i>" end)
-  end
-
-  def enclose(str) do
-    "<p>" <> str <> "</p>"
+  defp build_tag(body, tag) do
+    "<#{tag}>" <> body <> "</#{tag}>"
   end
 end
